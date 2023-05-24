ad57b5e8a06923d3e470c2c09824251ee782ce58
Jiaming Pei <pjming00006@gmail.com>
2023-05-24 11:57:12 -0700

    Wikimedia project setup

---
+++ Kafka-beginner/kafka-producer-wikimedia/src/main/java/io/conduktor/demo/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,7 @@
+package io.conduktor.demo.kafka.wikimedia;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) {
+
+    }
+}
--- Kafka-beginner/settings.gradle.kts
+++ Kafka-beginner/settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "Kafka-beginner"
 include("kafka-basics")
+include("kafka-producer-wikimedia")
